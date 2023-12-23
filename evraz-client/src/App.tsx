0b8d3d1c30d42dbc5e4d9b405ae9a0fcfe9c9d2f
NeAndrew <andrey1mosckalenko@gmail.com>
2023-12-23 03:55:39 +0300

    add tool-tip

--- evraz-client/src/App.tsx
+++ evraz-client/src/App.tsx
@@ -2,11 +2,14 @@ import { StationPage } from "./pages/station-page/station-page";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-bootstrap";
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
       <StationPage />
+      <Tooltip id="train-info" />
     </DndProvider>
   );
 }
