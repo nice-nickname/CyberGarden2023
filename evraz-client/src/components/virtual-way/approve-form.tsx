b29b4220afa156d05f62b91a1537e3572054a4c4
NeAndrew <andrey1mosckalenko@gmail.com>
2023-12-24 08:20:18 +0300

    refetch

--- evraz-client/src/components/virtual-way/approve-form.tsx
+++ evraz-client/src/components/virtual-way/approve-form.tsx
@@ -1,8 +1,9 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { Button, Form, Modal } from "react-bootstrap";
 import { baseUrl } from "../../consts";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export interface IApproveFormProps {
     id: number,
@@ -13,11 +14,14 @@ export interface IApproveFormProps {
 export function ApproveForm({ id, state, onClose }: IApproveFormProps) {
 
     const [value, setValue] = useState('')
+    const firstTry = useRef(true)
+
+    const queryClient = useQueryClient()
 
     const reviewForm = useMutation({
         mutationKey: ['review-form', id],
         mutationFn: async() => {
-          const response = await axios.post(`${baseUrl}forms/formReview`, { id, accepted: true, comment:  value, reviewerId: 0});
+          const response = await axios.post(`${baseUrl}forms/formReview`, { formId: id, accepted: true, comment:  value, reviewerId: 0});
           return response.data;
         }
     })
@@ -25,6 +29,14 @@ export function ApproveForm({ id, state, onClose }: IApproveFormProps) {
     const onClick = useCallback(() => {
         reviewForm.mutate()
     }, [reviewForm])
+
+    useEffect(() => {
+        if(!reviewForm.isPending && !firstTry.current) {
+            queryClient.refetchQueries();
+            onClose();
+        }
+        firstTry.current = false
+    }, [reviewForm.isPending, queryClient])
 
     return (
         <Modal show={state} onHide={onClose}>
